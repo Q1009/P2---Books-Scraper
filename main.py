ab53c01f42ee9f0c901fbc3c9d4866947f5cdd0e
Quentin Tellier <quentin.tellier@edu.ece.fr>
2025-06-02 17:20:25 +0200

    Modification de la fonction qui transforme les données extraites et de la fonction qui écrit dans un fichier csv les infos de tous les articles de la catégorie

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+import os
 
 # Instance of object type str as the url of the website
 url : str = 'https://books.toscrape.com/'
 url_fantasy : str = 'https://books.toscrape.com/catalogue/category/books/fantasy_19/index.html'
 
-
-def extract_article_links(url : str = 'https://books.toscrape.com/') -> list :
+def extract_article_links(url : str) -> list : #Etre précis dans le typage, liste de quoi ?
     # Intance of object type list to hold all links
     links : list = []
     # Getting rid of index.html and replacing it by page-i.html
     i = 1
-    url_category_page = url[:-10] + 'page-' + str(i) + '.html'
+    url_category_page : str = url[:-10] + 'page-' + str(i) + '.html'
     # Instance of object type requests.models.Response
     r : requests.models.Response = requests.get(url_category_page)
 
@@ -40,18 +40,18 @@ def get_article_data(): # incomplete_link : str = 'https://books.toscrape.com/'
     with open ('article_urls_output.txt', 'r') as file_txt:
         for row in file_txt:
             complete_link = url + row.strip()
-            response = requests.get(complete_link)
+            response = requests.get(complete_link) # Le goulot est là (en termes de rapidité d'exécution)
 
             if response.status_code == 200:
                 extracted_data = {}
                 extracted_data['product_page_url'] = complete_link
-                article_soup = BeautifulSoup(response.text, 'html.parser')
+                article_soup = BeautifulSoup(response.text, 'html.parser') # passer en parametre une option d'encoding UTF 8
                 extracted_data['title'] = article_soup.find('article').find('h1').text
                 rating = article_soup.find('article').find('div', class_ = 'col-sm-6 product_main').find_all('p')
                 rating2 = rating[2]
                 rating3 = rating2['class']
                 rating4 = rating3[1]
-                extracted_data['review_rating'] = rating4
+                extracted_data['review_rating'] = rating4 # convertir en chiffre () avec une table de conversion
                 description = article_soup.find('article').find_all('p')
                 extracted_data['product_description'] = description[3].text
                 image_url = article_soup.find('article').find('img')
@@ -74,8 +74,8 @@ def get_article_data(): # incomplete_link : str = 'https://books.toscrape.com/'
 
 def transform_article_data(data_to_transform):
     transformed_article_data = []
-    transformed_data = {}
     for data in data_to_transform:
+        transformed_data = {}
         transformed_data['product_page_url'] = data['product_page_url']
         transformed_data['universal_product_code'] = data['upc']
         transformed_data['title'] = data['title']
@@ -93,7 +93,7 @@ def transform_article_data(data_to_transform):
 
     return transformed_article_data
      
-def load_article_data(data_to_load):
+def load_article_data(data_to_load): # Mettre un nom de fichier par catégorie (os mkdir pour le répertoire)
     # data to load is a list of dictionaries
     with open('article_data_output.csv', 'w', newline='') as file_csv:
         header = (data_to_load[0].keys())
@@ -109,18 +109,18 @@ def load_article_urls(urls_to_load : list = []):
 
 
 """
+
+Créer un dossier par catégorie -> on y retrouve le csv et toutes les images
 #main
-
-link = get_article_link(url)
-article_data_to_transform = get_article_data(link)
-article_data_to_load = transform_article_data(article_data_to_transform)
-load_article_data(article_data_to_load)
-print("data loaded")
-
-"""
 
 #article_urls_to_load = extract_article_links(url_fantasy)
 #load_article_urls(article_urls_to_load)
 article_data_to_transform = get_article_data()
-print(len(article_data_to_transform))
-print(article_data_to_transform[0])
+article_data_to_load = transform_article_data(article_data_to_transform)
+print(len(article_data_to_load))
+#print(article_data_to_load[0])
+#load_article_data(article_data_to_load)
+print("data loaded")
+
+"""
+
